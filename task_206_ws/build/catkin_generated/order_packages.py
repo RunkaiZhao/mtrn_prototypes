410d4d072fceb376c79007cdcde639e9f507bd05
Nik-Medimurac <nikola.medimurac@gmail.com>
2020-07-04 11:52:00 +1000

    setting up task 206 workspace

---
+++ task_206_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/owr/Documents/owr/task_206/task_206_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
---
+++ task_206_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/owr/Documents/owr/task_206/task_206_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
